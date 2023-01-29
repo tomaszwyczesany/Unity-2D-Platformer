dcb107ee7a50f9d76d0e7b8390ceb7ee1ef57180
DESKTOP-5J7JC27\Administrator <tomaszwyczesany@gmail.com>
2023-01-29 19:44:43 +0100

    Initial commit

---
+++ Assets/PhysicsObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifier = 1f;
+    protected Vector2 velocity;
+    protected Rigidbody2D rb2d;
+    void OnEnable() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate() {
+        velocity += gravityModifier * Physics2D.gravity *Time.deltaTime;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 move = Vector2.up * deltaPosition.y;
+        Movement(move);
+    }
+    void Movement(Vector2 move){
+        rb2d.position = rb2d.position + move;
+    }
+}
